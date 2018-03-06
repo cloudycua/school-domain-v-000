559c2e684e684fe59f91b899cbdfa66d28662bf1
Christine Cua <cloudycua@gmail.com>
2018-03-06 17:44:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
   end
 
   def add_student(student, grade)
-    hash[grade] = []
-    hash[grade] << student
+    roster[grade] = []
+    roster[grade] << student
     @roster << hash
   end
 
